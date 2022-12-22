0c693cdef7387025af5bee6a31ab3e18f94ade54
Gregg Fine <gregg@greggfinedev.com>
2022-12-21 21:55:27 -0500

    Updates

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const userSelectedColor1 = document.querySelector("#userselected-color-1");
 const userSelectedColor2 = document.querySelector("#userselected-color-2");
 const localStorageDisplay = document.querySelector("#code");
 const removeCodeBtn = document.querySelector("#remove-code-btn");
+const paletteContainer = document.getElementById("palette-1");
+const paletteContainer2 = document.getElementById("palette-2");
 let selectedCompany = "";
 let hexColorArr = [];
 
@@ -18,27 +20,68 @@ removeCodeBtn.addEventListener("click", () => {
   }
 });
 
+const loadBtn = document.querySelector("#btnLoad");
+loadBtn.addEventListener("click", () => {
+  main();
+});
+
 const companies = [
-  "Alkemy X",
-  "Fellow",
-  "Gradient Pictures",
-  "Kroma Digital Cosmetics",
-  "Picture North",
-  "Tessa Films",
+  "alkemy-x",
+  "fellow",
+  "gradient-pictures",
+  "kroma-digital-cosmetics",
+  "picture-north",
+  "tessa-films",
 ];
+
+window.onload = () => {
+  console.clear();
+  // loadBtn.disabled = true;
+  getFromLocalStorage();
+  selectedCompany = companies[0];
+  const color1 = localStorage.getItem(`${selectedCompany}1`);
+  const color2 = localStorage.getItem(`${selectedCompany}2`);
+  colorStop1.style.stopColor = JSON.parse(color1);
+  colorStop2.style.stopColor = JSON.parse(color2);
+  // const canvas = document.getElementById("canvas");
+  // const ctx = canvas.getContext("2d");
+  // const image = new Image();
+  // let imgStr = `${selectedCompany}IMG`;
+  // image.src = localStorage.getItem(imgStr);
+  // window.setTimeout(() => {
+  //   canvas.width = image.width;
+  //   canvas.height = image.height;
+  //   ctx.drawImage(image, 0, 0);
+  // }, 1);
+};
+
 companySelector.addEventListener("change", (e) => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  document.getElementById("palette").innerHTML = "";
+  document.getElementById("palette-1").innerHTML = "";
   document.getElementById("palette-2").innerHTML = "";
   selectedCompany = e.target.value;
   const color1 = localStorage.getItem(`${selectedCompany}1`);
   const color2 = localStorage.getItem(`${selectedCompany}2`);
   colorStop1.style.stopColor = JSON.parse(color1);
   colorStop2.style.stopColor = JSON.parse(color2);
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  // const image = new Image();
+  // if (selectedCompany) {
+  //   let imgStr = `${selectedCompany}IMG`;
+  //   image.src = localStorage.getItem(imgStr);
+  //   window.setTimeout(() => {
+  //     canvas.width = image.width;
+  //     canvas.height = image.height;
+  //     ctx.drawImage(image, 0, 0);
+  //   }, 1);
+  // }
+  // else {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // }
+  // console.log(localStorage.getItem("fellowIMG"));
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
-getFromLocalStorage();
+// getFromLocalStorage();
 
 function getFromLocalStorage() {
   //   const items = { ...localStorage };
@@ -67,235 +110,6 @@ function getFromLocalStorage() {
   };`;
 }
 
-const buildPalette = (colorsList) => {
-  hexColorArr = [];
-  const paletteContainer = document.getElementById("palette");
-  const paletteContainer2 = document.getElementById("palette-2");
-  paletteContainer.innerHTML = "";
-  paletteContainer2.innerHTML = "";
-  const orderedByColor = orderByLuminance(colorsList);
-
-  for (let i = 0; i < orderedByColor.length; i++) {
-    const hexColor = rgbToHex(orderedByColor[i]);
-    hexColorArr.push(hexColor);
-
-    const colorElement = document.createElement("div");
-    colorElement.className = "color-element";
-    colorElement.style.backgroundColor = hexColor;
-    if (hexColor !== "#FFFFFF") {
-      colorElement.appendChild(document.createTextNode(hexColor));
-    }
-
-    let colorElement2 = colorElement.cloneNode(true);
-
-    colorElement.addEventListener("click", (e) => {
-      if (localStorage.getItem(`${selectedCompany}1`)) {
-        const colorElements1 = document.querySelectorAll(
-          "#palette .color-element"
-        );
-        colorElements1.forEach((colorEl) => {
-          if (colorEl.textContent === e.target.textContent) {
-            colorEl.style.border = "3px solid #000";
-            colorEl.style.borderRadius = "50%";
-          } else {
-            colorEl.style.border = "none";
-            colorEl.style.borderRadius = "0%";
-          }
-        });
-        let selectedColor = e.target.textContent;
-        colorStop1.style.stopColor = selectedColor;
-        localStorage.setItem(
-          `${selectedCompany}1`,
-          `"${e.target.textContent}"`
-        );
-      } else {
-        let selectedColor = e.target.textContent;
-        colorStop1.style.stopColor = selectedColor;
-        localStorage.setItem(
-          `${selectedCompany}1`,
-          `"${e.target.textContent}"`
-        );
-      }
-      getFromLocalStorage();
-    });
-
-    colorElement2.addEventListener("click", (e) => {
-      if (localStorage.getItem(`${selectedCompany}1`)) {
-        const colorElements2 = document.querySelectorAll(
-          "#palette-2 .color-element"
-        );
-        colorElements2.forEach((colorEl) => {
-          if (colorEl.textContent === e.target.textContent) {
-            colorEl.style.border = "3px solid #000";
-            colorEl.style.borderRadius = "50%";
-          } else {
-            colorEl.style.border = "none";
-            colorEl.style.borderRadius = "0%";
-          }
-        });
-        let selectedColor = e.target.textContent;
-        colorStop2.style.stopColor = selectedColor;
-        localStorage.setItem(
-          `${selectedCompany}2`,
-          `"${e.target.textContent}"`
-        );
-      } else {
-        let selectedColor = e.target.textContent;
-        colorStop2.style.stopColor = selectedColor;
-        localStorage.setItem(
-          `${selectedCompany}2`,
-          `"${e.target.textContent}"`
-        );
-      }
-      getFromLocalStorage();
-    });
-    if (colorElement.style.backgroundColor != "rgb(255, 255, 255)") {
-      paletteContainer.appendChild(colorElement);
-      paletteContainer2.appendChild(colorElement2);
-    }
-  }
-  colorArrFinal = Array.from(new Set(hexColorArr));
-};
-
-//  Convert each pixel value ( number ) to hexadecimal ( string ) with base 16
-const rgbToHex = (pixel) => {
-  const componentToHex = (c) => {
-    const hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  };
-
-  return (
-    "#" +
-    componentToHex(pixel.r) +
-    componentToHex(pixel.g) +
-    componentToHex(pixel.b)
-  ).toUpperCase();
-};
-
-/**
- * Using relative luminance we order the brightness of the colors
- * the fixed values and further explanation about this topic
- * can be found here -> https://en.wikipedia.org/wiki/Luma_(video)
- */
-const orderByLuminance = (rgbValues) => {
-  const calculateLuminance = (p) => {
-    return 0.2126 * p.r + 0.7152 * p.g + 0.0722 * p.b;
-  };
-
-  return rgbValues.sort((p1, p2) => {
-    return calculateLuminance(p2) - calculateLuminance(p1);
-  });
-};
-
-const buildRgb = (imageData) => {
-  const rgbValues = [];
-  // note that we are loopin every 4!
-  // for every Red, Green, Blue and Alpha
-  for (let i = 0; i < imageData.length; i += 4) {
-    const rgb = {
-      r: imageData[i],
-      g: imageData[i + 1],
-      b: imageData[i + 2],
-    };
-
-    rgbValues.push(rgb);
-  }
-
-  return rgbValues;
-};
-
-// returns what color channel has the biggest difference
-const findBiggestColorRange = (rgbValues) => {
-  /**
-   * Min is initialized to the maximum value posible
-   * from there we procced to find the minimum value for that color channel
-   *
-   * Max is initialized to the minimum value posible
-   * from there we procced to fin the maximum value for that color channel
-   */
-  let rMin = Number.MAX_VALUE;
-  let gMin = Number.MAX_VALUE;
-  let bMin = Number.MAX_VALUE;
-
-  let rMax = Number.MIN_VALUE;
-  let gMax = Number.MIN_VALUE;
-  let bMax = Number.MIN_VALUE;
-
-  rgbValues.forEach((pixel) => {
-    rMin = Math.min(rMin, pixel.r);
-    gMin = Math.min(gMin, pixel.g);
-    bMin = Math.min(bMin, pixel.b);
-
-    rMax = Math.max(rMax, pixel.r);
-    gMax = Math.max(gMax, pixel.g);
-    bMax = Math.max(bMax, pixel.b);
-  });
-
-  const rRange = rMax - rMin;
-  const gRange = gMax - gMin;
-  const bRange = bMax - bMin;
-
-  // determine which color has the biggest difference
-  const biggestRange = Math.max(rRange, gRange, bRange);
-  if (biggestRange === rRange) {
-    return "r";
-  } else if (biggestRange === gRange) {
-    return "g";
-  } else {
-    return "b";
-  }
-};
-
-/**
- * Median cut implementation
- * can be found here -> https://en.wikipedia.org/wiki/Median_cut
- */
-const quantization = (rgbValues, depth) => {
-  const MAX_DEPTH = 4;
-
-  // Base case
-  if (depth === MAX_DEPTH || rgbValues.length === 0) {
-    const color = rgbValues.reduce(
-      (prev, curr) => {
-        prev.r += curr.r;
-        prev.g += curr.g;
-        prev.b += curr.b;
-
-        return prev;
-      },
-      {
-        r: 0,
-        g: 0,
-        b: 0,
-      }
-    );
-
-    color.r = Math.round(color.r / rgbValues.length);
-    color.g = Math.round(color.g / rgbValues.length);
-    color.b = Math.round(color.b / rgbValues.length);
-
-    return [color];
-  }
-
-  /**
-   *  Recursively do the following:
-   *  1. Find the pixel channel (red,green or blue) with biggest difference/range
-   *  2. Order by this channel
-   *  3. Divide in half the rgb colors list
-   *  4. Repeat process again, until desired depth or base case
-   */
-  const componentToSortBy = findBiggestColorRange(rgbValues);
-  rgbValues.sort((p1, p2) => {
-    return p1[componentToSortBy] - p2[componentToSortBy];
-  });
-
-  const mid = rgbValues.length / 2;
-  return [
-    ...quantization(rgbValues.slice(0, mid), depth + 1),
-    ...quantization(rgbValues.slice(mid + 1), depth + 1),
-  ];
-};
-
 const main = () => {
   const imgFile = document.getElementById("imgfile");
   const image = new Image();
@@ -304,6 +118,7 @@ const main = () => {
 
   // Whenever file & image is loaded procced to extract the information from the image
   fileReader.onload = () => {
+    // loadBtn.disabled = false;
     image.onload = () => {
       // Set the canvas size to be the same as of the uploaded image
       const canvas = document.getElementById("canvas");
@@ -319,6 +134,17 @@ const main = () => {
        * the alpha is not from 0 to 1 like it is in the RGBA of CSS, but from 0 to 255.
        */
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+      /* ======================================================= */
+      /* ======================================================= */
+      /* ======================================================= */
+      /* GREGG IMAGE EXPERIMENT!!!!! */
+      // let imgAsDataURL = canvas.toDataURL("image/jpg");
+      // localStorage.setItem(`${selectedCompany}IMG`, imgAsDataURL);
+      /* GREGG IMAGE EXPERIMENT END!!!!! */
+      /* ======================================================= */
+      /* ======================================================= */
+      /* ======================================================= */
 
       // Convert the image data to RGB values so its much simpler
       const rgbArray = buildRgb(imageData.data);
@@ -339,12 +165,85 @@ const main = () => {
     image.src = fileReader.result;
   };
   fileReader.readAsDataURL(file);
+  imgFile.value = "";
 };
 
-const loadBtn = document.querySelector("#btnLoad");
-loadBtn.addEventListener("click", () => {
-  main();
-});
+const buildPalette = (colorsList) => {
+  hexColorArr = [];
+  paletteContainer.innerHTML = "";
+  paletteContainer2.innerHTML = "";
+  const orderedByColor = orderByLuminance(colorsList);
+
+  for (let i = 0; i < orderedByColor.length; i++) {
+    const hexColor = rgbToHex(orderedByColor[i]);
+    hexColorArr.push(hexColor);
+
+    const colorElement = document.createElement("div");
+    colorElement.className = "color-element";
+    colorElement.style.backgroundColor = hexColor;
+    if (hexColor !== "#FFFFFF") {
+      colorElement.appendChild(document.createTextNode(hexColor));
+    }
+
+    let colorElement2 = colorElement.cloneNode(true);
+
+    function visiblyIndicateColor(colorElements, selectedColor) {
+      // loop through the NodeList and, if the element's hex code matches the color that was clicked on
+      // visually indicate it with a circular border
+      // Else, reset the border style
+      colorElements.forEach((colorEl) => {
+        if (colorEl.textContent === selectedColor) {
+          colorEl.style.border = "3px solid #000";
+          colorEl.style.borderRadius = "50%";
+        } else {
+          colorEl.style.border = "none";
+          colorEl.style.borderRadius = "0%";
+        }
+      });
+    }
+
+    function setColorStopsInLogo(colorPaletteNum, selectedColor) {
+      // set the VLM Logo's colorStop1
+      if (colorPaletteNum === 1) {
+        colorStop1.style.stopColor = selectedColor;
+      } else if (colorPaletteNum === 2) {
+        colorStop2.style.stopColor = selectedColor;
+      }
+    }
+
+    function saveColorToLocalStorage(colorPaletteNum, selectedColor) {
+      localStorage.setItem(
+        `${selectedCompany}${colorPaletteNum}`,
+        `"${selectedColor}"`
+      );
+    }
+
+    function handleColorPalette(e, colorPaletteNum) {
+      // get the hex code for the color that was clicked on
+      let selectedColor = e.target.textContent;
+      // get a NodeList of all the color elements depending on which palette was clicked on
+      const colorElements = document.querySelectorAll(
+        `#palette-${colorPaletteNum} .color-element`
+      );
+      visiblyIndicateColor(colorElements, selectedColor);
+      setColorStopsInLogo(colorPaletteNum, selectedColor);
+      saveColorToLocalStorage(colorPaletteNum, selectedColor);
+      getFromLocalStorage();
+    }
+
+    colorElement.addEventListener("click", (e) => {
+      handleColorPalette(e, 1);
+    });
+    colorElement2.addEventListener("click", (e) => {
+      handleColorPalette(e, 2);
+    });
+    if (colorElement.style.backgroundColor != "rgb(255, 255, 255)") {
+      paletteContainer.appendChild(colorElement);
+      paletteContainer2.appendChild(colorElement2);
+    }
+  }
+  colorArrFinal = Array.from(new Set(hexColorArr));
+};
 
 //   Gregg extra code to download the new SVG
 // https://stackoverflow.com/questions/60241398/how-to-download-and-svg-element-as-an-svg-file
