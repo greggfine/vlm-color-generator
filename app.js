91041628eb9ab5822244fefbae7451d06bb62dbd
Gregg Fine <gregg@greggfinedev.com>
2022-12-22 16:40:02 -0500

    Updates

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const companies = [
   "picture-north",
   "tessa-films",
 ];
+const loadBtn = document.querySelector("#btnLoad");
 const companySelector = document.querySelector("#company-selector");
 const currentCompany = document.querySelector("#current-company");
 const vlmGradient = document.querySelector("#vlm-gradient");
@@ -35,10 +36,13 @@ removeThisCodeBtn.addEventListener("click", () => {
   if (answer) {
     localStorage.removeItem(`${selectedCompany}1`);
     localStorage.removeItem(`${selectedCompany}2`);
+    localStorage.removeItem(`${selectedCompany}-hexcodes`);
+    getCodeFromLocalStorage();
     colorStop1.style.stopColor = "#000";
     colorStop2.style.stopColor = "#000";
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    getCodeFromLocalStorage();
+    removeAllChildNodes(paletteContainer);
+    removeAllChildNodes(paletteContainer2);
   }
 });
 
@@ -58,30 +62,19 @@ function removeAllChildNodes(parent) {
   }
 }
 
-const loadBtn = document.querySelector("#btnLoad");
-loadBtn.addEventListener("click", () => {
-  main();
-});
-
 window.onload = () => {
   console.clear();
   // loadBtn.disabled = true;
+  // loadBtn.style.disabled = "true";
   getCodeFromLocalStorage();
   selectedCompany = companies[0];
   const color1 = localStorage.getItem(`${selectedCompany}1`);
   const color2 = localStorage.getItem(`${selectedCompany}2`);
   colorStop1.style.stopColor = JSON.parse(color1);
   colorStop2.style.stopColor = JSON.parse(color2);
-  // const canvas = document.getElementById("canvas");
-  // const ctx = canvas.getContext("2d");
-  // const image = new Image();
-  // let imgStr = `${selectedCompany}IMG`;
-  // image.src = localStorage.getItem(imgStr);
-  // window.setTimeout(() => {
-  //   canvas.width = image.width;
-  //   canvas.height = image.height;
-  //   ctx.drawImage(image, 0, 0);
-  // }, 1);
+  loadBtn.addEventListener("click", () => {
+    main();
+  });
 };
 
 companySelector.addEventListener("change", (e) => {
@@ -92,28 +85,88 @@ companySelector.addEventListener("change", (e) => {
   const color2 = localStorage.getItem(`${selectedCompany}2`);
   colorStop1.style.stopColor = JSON.parse(color1);
   colorStop2.style.stopColor = JSON.parse(color2);
-  // const canvas = document.getElementById("canvas");
-  // const ctx = canvas.getContext("2d");
-  // const image = new Image();
-  // if (selectedCompany) {
-  //   let imgStr = `${selectedCompany}IMG`;
-  //   image.src = localStorage.getItem(imgStr);
-  //   window.setTimeout(() => {
-  //     canvas.width = image.width;
-  //     canvas.height = image.height;
-  //     ctx.drawImage(image, 0, 0);
-  //   }, 1);
-  // }
-  // else {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // }
-  // console.log(localStorage.getItem("fellowIMG"));
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // const hexArray = JSON.parse(
+  //   localStorage.getItem(`${selectedCompany}-hexcodes`)
+  // );
+  const stringArr = localStorage.getItem(`${selectedCompany}-hexcodes`);
+  const splitStringArr = stringArr.split(",");
+  // Now, we have an array of hex values
+  // We need to build the palettes
+  splitStringArr.forEach((hexColor) => {
+    const colorElement = document.createElement("div");
+    colorElement.className = "color-element";
+    colorElement.style.backgroundColor = hexColor;
+    colorElement.appendChild(document.createTextNode(hexColor));
+    let colorElement2 = colorElement.cloneNode(true);
+    colorElement.addEventListener("click", (e) => {
+      handleColorPalette(e, 1);
+    });
+    colorElement2.addEventListener("click", (e) => {
+      handleColorPalette(e, 2);
+    });
+    if (colorElement.style.backgroundColor != "rgb(255, 255, 255)") {
+      paletteContainer.appendChild(colorElement);
+      paletteContainer2.appendChild(colorElement2);
+    }
+    if (colorElement.textContent === JSON.parse(color1)) {
+      colorElement.style.border = "3px solid #000";
+      colorElement.style.borderRadius = "50%";
+    }
+    if (colorElement2.textContent === JSON.parse(color2)) {
+      colorElement2.style.border = "3px solid #000";
+      colorElement2.style.borderRadius = "50%";
+    }
+  });
 });
-// getFromLocalStorage();
+
+function visiblyIndicateColor(colorElements, selectedColor) {
+  // loop through the NodeList and, if the element's hex code matches the color that was clicked on
+  // visually indicate it with a circular border
+  // Else, reset the border style
+  colorElements.forEach((colorEl) => {
+    if (colorEl.textContent === selectedColor) {
+      colorEl.style.border = "3px solid #000";
+      colorEl.style.borderRadius = "50%";
+    } else {
+      colorEl.style.border = "none";
+      colorEl.style.borderRadius = "0%";
+    }
+  });
+}
+function setColorStopsInLogo(colorPaletteNum, selectedColor) {
+  if (colorPaletteNum === 1) {
+    colorStop1.style.stopColor = selectedColor;
+  } else if (colorPaletteNum === 2) {
+    colorStop2.style.stopColor = selectedColor;
+  }
+}
+
+function saveColorToLocalStorage(colorPaletteNum, selectedColor) {
+  localStorage.setItem(
+    `${selectedCompany}${colorPaletteNum}`,
+    `"${selectedColor}"`
+  );
+}
+function handleColorPalette(e, colorPaletteNum) {
+  // get the hex code for the color that was clicked on
+  let selectedColor = e.target.textContent;
+  // get a NodeList of all the color elements depending on which palette was clicked on
+  const colorElements = document.querySelectorAll(
+    `#palette-${colorPaletteNum} .color-element`
+  );
+  visiblyIndicateColor(colorElements, selectedColor);
+  setColorStopsInLogo(colorPaletteNum, selectedColor);
+  saveColorToLocalStorage(colorPaletteNum, selectedColor);
+  getCodeFromLocalStorage();
+}
+
+// console.log(splitStringArr);
+// console.log(hexArray);
+// console.log(typeof stringArr);
+// });
 
 function getCodeFromLocalStorage() {
-  //   const items = { ...localStorage };
   let stringifiedBlack = JSON.stringify("#000");
   localStorageDisplay.textContent = `const colors = {
 	alkemyX1: ${localStorage.getItem("alkemy-x1") || stringifiedBlack},
@@ -144,10 +197,20 @@ const main = () => {
   const image = new Image();
   const file = imgFile.files[0];
   const fileReader = new FileReader();
+  // loadBtn.disabled = false;
+  // console.log(imgFile);
+  // imgFile.addEventListener("click", function () {
+  //   console.log("click");
+  //   // this.value = null;
+  //   // loadBtn.disabled = false;
+  // });
+  // imgFile.addEventListener("change", function () {
+  //   console.log("hey");
+  //   loadBtn.disabled = false;
+  // });
 
   // Whenever file & image is loaded procced to extract the information from the image
   fileReader.onload = () => {
-    // loadBtn.disabled = false;
     image.onload = () => {
       // Set the canvas size to be the same as of the uploaded image
       // const canvas = document.getElementById("canvas");
@@ -230,7 +293,6 @@ const buildPalette = (colorsList) => {
     }
 
     function setColorStopsInLogo(colorPaletteNum, selectedColor) {
-      // set the VLM Logo's colorStop1
       if (colorPaletteNum === 1) {
         colorStop1.style.stopColor = selectedColor;
       } else if (colorPaletteNum === 2) {
@@ -270,6 +332,7 @@ const buildPalette = (colorsList) => {
     }
   }
   colorArrFinal = Array.from(new Set(hexColorArr));
+  localStorage.setItem(`${selectedCompany}-hexcodes`, colorArrFinal);
 };
 
 //   Gregg extra code to download the new SVG
